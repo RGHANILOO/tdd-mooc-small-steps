54da912a459fb0ac82ca23a714ca1a7475fc3282
“rezan5910” <bytemefirst@gmail.com>
2024-04-27 15:47:51 +0100

    holiday update to temporeal

--- src/prices.mjs
+++ src/prices.mjs
@@ -85,7 +85,7 @@ function createApp(database) {
   function isHoliday(date) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
-      let holiday = new Date(row.holiday);
+      let holiday = new Temporal.PlainDate(row.holiday);
       if (
         date &&
         date.getFullYear() === holiday.getFullYear() &&
